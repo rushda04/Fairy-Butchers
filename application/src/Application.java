ad7720ceff7cfa5b24617cedbcba7f93f8befbbb
Botezan-Marius-Damian <563685@student.saxion.nl>
2025-11-24 14:10:57 +0100

    Scaling function added

--- application/src/Application.java
+++ application/src/Application.java
@@ -9,9 +9,10 @@ public class Application implements Runnable {
     BufferedImage fairyScaled;
     BufferedImage playerScaled;
     String fairyScaledPath = "resources/fairy_scaled.png";
-    playerScaledPath = "resources/player_scaled.png";
+    String playerScaledPath = "resources/player_scaled.png";
 
     public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1.0");
         SaxionApp.start(new Application(), 1536, 1024);
     }
 
@@ -19,7 +20,6 @@ public class Application implements Runnable {
         loadSprites();
         intro();
         enterGame();
-        gameHUD();
     }
 
     public void intro() {
@@ -33,27 +33,14 @@ public class Application implements Runnable {
 
         // Draw scaled fairy sprite
         SaxionApp.drawImage(fairyScaledPath, 700, 700);
+        SaxionApp.drawImage(playerScaledPath, 200, 700);
     }
 
-    public void gameHUD() {
-        SaxionApp.drawRectangle(20, 20, 500, 20);
-        SaxionApp.setFill(Color.blue);
-        SaxionApp.drawRectangle(20, 47, 700, 10);
-        SaxionApp.drawLine(0, 700, 1260, 700);
-        SaxionApp.drawRectangle(470, 760, 60, 60);
-        SaxionApp.drawRectangle(540, 760, 60, 60);
-        SaxionApp.drawRectangle(610, 760, 60, 60);
-        SaxionApp.drawRectangle(680, 760, 60, 60);
-        SaxionApp.setFill(Color.magenta);
-        SaxionApp.drawRectangle(1250, 100, 25, 500);
-        SaxionApp.setFill(Color.GRAY);
-        SaxionApp.drawRectangle(1180, 15, 50, 50);
-    }
 
     public void loadSprites() {
         // Keep your existing hard-coded call but use the generalized function
-        fairyScaled = loadSprite("resources/FairyNo2.png", fairyScaledPath, 4);
-        playerScaled = loadSprite("resources/quake E.png", playerScaledPath, 4);
+        fairyScaled = loadSprite("resources/FairyNo2.png", fairyScaledPath, 8);
+        playerScaled = loadSprite("resources/quake E.png", playerScaledPath, 10);
     }
 
     // Generalized sprite loader function
