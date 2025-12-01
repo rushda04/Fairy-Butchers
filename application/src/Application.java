4d83852d92f8315b94c76cd60b60d9343ee0d21a
Damimarius18 <104790321+Damimarius18@users.noreply.github.com>
2025-12-01 14:34:25 +0100

    Play button finished

--- application/src/Application.java
+++ application/src/Application.java
@@ -1,72 +1,203 @@
 import nl.saxion.app.SaxionApp;
+import nl.saxion.app.interaction.GameLoop;
+import nl.saxion.app.interaction.KeyboardEvent;
+import nl.saxion.app.interaction.MouseEvent;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.File;
 
-public class Application implements Runnable {
+public class Application implements GameLoop {
 
     BufferedImage fairyScaled;
     BufferedImage playerScaled;
+    BufferedImage playButtonScaled;
+    BufferedImage playButtonHoverScaled;
+
+
+
     String fairyScaledPath = "resources/fairy_scaled.png";
     String playerScaledPath = "resources/player_scaled.png";
+    String playButtonScaledPath = "resources/playButton_scaled.png";
+    String playButtonHoverScaledPath = "resources/playButton_hover_scaled.png";
+
+    boolean hoveringPlayButton = false;
+
+
+
+
+    int playButtonHoverWidth = 0;
+    int playButtonHoverHeight = 0;
+
+
+    int playButtonX = 450;
+    int playButtonY = 550;
+    int playButtonWidth = 0;
+    int playButtonHeight = 0;
+
+    boolean inIntro = true;
 
     public static void main(String[] args) {
         System.setProperty("sun.java2d.uiScale", "1.0");
-        SaxionApp.start(new Application(), 1536, 1024);
+        SaxionApp.startGameLoop(new Application(), 1536, 1024, 16);
     }
 
-    public void run() {
+
+    @Override
+    public void init() {
         loadSprites();
-        intro();
-        enterGame();
     }
 
-    public void intro() {
+    @Override
+    public void loop() {
+        if (inIntro) {
+            drawIntro();
+        } else {
+            drawGame();
+        }
+    }
+
+    @Override
+    public void keyboardEvent(KeyboardEvent e) {
+        if (inIntro) {
+            inIntro = false;
+            SaxionApp.printLine("Started by keyboard event: " + e.getKeyCode());
+        }
+    }
+
+    @Override
+    public void mouseEvent(MouseEvent e) {
+        int mx = e.getX();
+        int my = e.getY();
+
+        hoveringPlayButton = (mx >= playButtonX && mx <= playButtonX + playButtonWidth &&
+                my >= playButtonY && my <= playButtonY + playButtonHeight);
+
+        if (inIntro && e.isMouseUp() && e.isLeftMouseButton()) {
+            if (hoveringPlayButton) {
+                inIntro = false;
+                SaxionApp.printLine("Play button clicked at: " + mx + "," + my);
+            }
+        }
+    }
+
+
+
+    private void drawIntro() {
+        SaxionApp.clear();
         SaxionApp.drawImage("resources/fairyButchersFrontPage.png", 0, 0);
-        SaxionApp.pause();
+        introHud();
     }
 
-    public void enterGame() {
+    private void drawGame() {
         SaxionApp.clear();
         SaxionApp.drawImage("resources/backgroundPictureBattleground.png", 0, 0);
 
-        // Draw scaled fairy sprite
-        SaxionApp.drawImage(fairyScaledPath, 700, 700);
-        SaxionApp.drawImage(playerScaledPath, 200, 700);
+        SaxionApp.drawImage(fairyScaledPath, 970, 700);
+        SaxionApp.drawImage(playerScaledPath, 200, 670);
+
+        inGameHud();
     }
 
 
     public void loadSprites() {
-        // Keep your existing hard-coded call but use the generalized function
         fairyScaled = loadSprite("resources/FairyNo2.png", fairyScaledPath, 8);
         playerScaled = loadSprite("resources/quake E.png", playerScaledPath, 10);
+        playButtonScaled = loadSprite("resources/playButton.png", playButtonScaledPath, 8);
+        playButtonHoverScaled = loadSprite("resources/playButton.png", playButtonHoverScaledPath, 6);
+
+        if (playButtonScaled != null) {
+            playButtonWidth = playButtonScaled.getWidth();
+            playButtonHeight = playButtonScaled.getHeight();
+        } else {
+            playButtonWidth = 200;
+            playButtonHeight = 80;
+        }
+
+        if (playButtonHoverScaled != null) {
+            playButtonHoverWidth = playButtonHoverScaled.getWidth();
+            playButtonHoverHeight = playButtonHoverScaled.getHeight();
+        } else {
+            playButtonHoverWidth = playButtonWidth;
+            playButtonHoverHeight = playButtonHeight;
+        }
     }
 
-    // Generalized sprite loader function
     public BufferedImage loadSprite(String inputPath, String outputPath, int scale) {
         try {
             BufferedImage original = ImageIO.read(new File(inputPath));
-
             int w = original.getWidth() * scale;
             int h = original.getHeight() * scale;
 
             BufferedImage scaled = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
             Graphics2D g2 = scaled.createGraphics();
-            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                    RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
             g2.drawImage(original, 0, 0, w, h, null);
             g2.dispose();
 
-            // Save scaled version if output path is provided
-            if (outputPath != null && !outputPath.isEmpty()) {
-                ImageIO.write(scaled, "png", new File(outputPath));
-            }
 
+            ImageIO.write(scaled, "png", new File(outputPath));
             return scaled;
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            SaxionApp.printLine("Failed to load/scale sprite: " + inputPath);
+            ex.printStackTrace();
             return null;
         }
     }
+
+
+    public void inGameHud() {
+        SaxionApp.setFill(Color.lightGray);
+        SaxionApp.drawRectangle(10, 10, 500, 10);
+        SaxionApp.drawRectangle(10, 30, 250, 10);
+        SaxionApp.drawRectangle(1470, 110, 50, 750);
+        SaxionApp.drawRectangle(950, 680, 300, 10);
+        SaxionApp.drawRectangle(10, 110, 60, 60);
+        SaxionApp.drawRectangle(10, 180, 60, 60);
+        SaxionApp.drawRectangle(10, 250, 60, 60);
+        SaxionApp.drawRectangle(10, 320, 60, 60);
+
+        fillHealth(75);
+        fillMana(75);
+        fillCorruption(25);
+        fillEnemy(35);
+    }
+
+    public void fillHealth(int healthPoints) {
+        SaxionApp.setFill(Color.red);
+        int filler = (int)((healthPoints / 100.0) * 500);
+        SaxionApp.drawRectangle(10, 10, filler, 10);
+    }
+
+    public void fillMana(int manaPoints) {
+        SaxionApp.setFill(Color.blue);
+        int filler = (int)((manaPoints / 100.0) * 250);
+        SaxionApp.drawRectangle(10, 30, filler, 10);
+    }
+
+    public void fillCorruption(int corruptionLevel) {
+        SaxionApp.setFill(Color.magenta);
+        int filler = (int)((corruptionLevel / 100.0) * 750);
+        SaxionApp.drawRectangle(1470, 110, 50, filler);
+    }
+
+    public void fillEnemy(int hp) {
+        SaxionApp.setFill(Color.red);
+        int filler = (int)((hp / 100.0) * 300);
+        SaxionApp.drawRectangle(950, 680, filler, 10);
+    }
+
+    public void introHud() {
+        if (hoveringPlayButton && playButtonHoverScaled != null) {
+            int hx = playButtonX - (playButtonHoverWidth - playButtonWidth) / 2;
+            int hy = playButtonY - (playButtonHoverHeight - playButtonHeight) / 2;
+            SaxionApp.drawImage(playButtonHoverScaledPath, hx, hy);
+        } else {
+            SaxionApp.drawImage(playButtonScaledPath, playButtonX, playButtonY);
+        }
+    }
+
+
 }
